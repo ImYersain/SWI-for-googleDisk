5529f2214d431c65b2bfd4b136f9ba511a300173
Yersain <71192180+ImYersain@users.noreply.github.com>
2023-04-25 22:11:05 +0200

    Add files via upload

--- server/utils/checkAuth.js
+++ server/utils/checkAuth.js
@@ -5,7 +5,7 @@ export const checkAuth = (req, res, next) => {
     const token = (req.headers.authorization || '').replace(/Bearer\s?/, '');  //изначально здесь такая строка Bearer hjhjh32h323lkj нам нужно вырезать именно токен с помощью реплейс
     if(token){
         try {
-            const decoded = jwt.verify(token, process.env.JWT_SECRET || 'hjkshdcjh43jkkhk9sdsufj7');
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.userId = decoded.id;
             next();
         } catch (error) {
